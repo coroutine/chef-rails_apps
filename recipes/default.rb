f511958419d560126ae58a86646c7c9c1164f80e
Brad Montgomery <brad@bradmontgomery.net>
2012-03-07 14:58:20 -0600

    updated license

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: rails_apps 
 # Recipe:: default
 #
-# Copyright 2012, Coroutine
+# Copyright 2012, Coroutine LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
