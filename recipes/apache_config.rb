e6bedd6c05bd0a470c04b1bd3642a8678f835905
Brad Montgomery <brad@bradmontgomery.net>
2012-03-07 14:11:22 -0600

    killed an extraneious end

--- recipes/apache_config.rb
+++ recipes/apache_config.rb
@@ -25,36 +25,35 @@ node['rails_apps'].each do |dbag_item|
 end
 
 app_configs.each do |app|
-    # Set up directories for each stage
-    app['stages'].each do |stage_name, stage_data|
-      
-      deploy_user   = stage_data['deploy_user']
-      base_path     = "/home/#{deploy_user}/#{appname}/#{stage_name}"    
-      instance_name = [appname, stage_name].join("_")
+  # Set up directories for each stage
+  app['stages'].each do |stage_name, stage_data|
+    
+    deploy_user   = stage_data['deploy_user']
+    base_path     = "/home/#{deploy_user}/#{appname}/#{stage_name}"    
+    instance_name = [appname, stage_name].join("_")
 
-      # See the definitions directory for the "app_config" source. It is 
-      # a modified version of the "web_app" definition that is included 
-      # in Opscode's apache2 cookbook. It uses these values to generate the 
-      # config files for the specified site and place them in
-      # /etc/apache2/sites-available, optionally running a2ensite 
-      # or a2dissite based on the value of the "enable" parameter.
-      app_config(instance_name) do
-        docroot                   "#{base_path}/current/public"
-        rack_env                  stage_name
-        server_name               stage_data['hostname']
-        server_aliases            stage_data['aliases'] || []
-        redirect_from             stage_data['redirect_from']
-        template                  "apache.conf.erb" 
-        passenger_min_instances   stage_data['min_instances'] || 1
-        enable                    stage_data['enable']
-        enable_ssl                stage_data['enable_ssl']
-        ssl_port                  stage_data['ssl_port']
-        ssl_cert_file             stage_data['ssl_cert_file']
-        ssl_cert_key_file         stage_data['ssl_cert_key_file']
-        ssl_cert_chain_file       stage_data['ssl_cert_chain_file']
-      end
-    end 
-  end
+    # See the definitions directory for the "app_config" source. It is 
+    # a modified version of the "web_app" definition that is included 
+    # in Opscode's apache2 cookbook. It uses these values to generate the 
+    # config files for the specified site and place them in
+    # /etc/apache2/sites-available, optionally running a2ensite 
+    # or a2dissite based on the value of the "enable" parameter.
+    app_config(instance_name) do
+      docroot                   "#{base_path}/current/public"
+      rack_env                  stage_name
+      server_name               stage_data['hostname']
+      server_aliases            stage_data['aliases'] || []
+      redirect_from             stage_data['redirect_from']
+      template                  "apache.conf.erb" 
+      passenger_min_instances   stage_data['min_instances'] || 1
+      enable                    stage_data['enable']
+      enable_ssl                stage_data['enable_ssl']
+      ssl_port                  stage_data['ssl_port']
+      ssl_cert_file             stage_data['ssl_cert_file']
+      ssl_cert_key_file         stage_data['ssl_cert_key_file']
+      ssl_cert_chain_file       stage_data['ssl_cert_chain_file']
+    end
+  end 
 end
 
 # Disable Apache's default site
