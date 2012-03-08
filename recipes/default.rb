565edd26f54b55071c3881afb721a0e50c0a8a46
Brad Montgomery <brad@bradmontgomery.net>
2012-03-08 12:52:29 -0600

    added support for mod_xsendfile

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,6 +19,7 @@
 
 include_recipe "apache2::default"
 include_recipe "apache2::mod_expires"
+include_recipe "apache2::mod_xsendfile"
 include_recipe "rvm::system"
 include_recipe "rvm_passenger::default"
 include_recipe "rvm_passenger::apache2"
