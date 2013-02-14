41de0f418145faeeb1a6520818db79616a0d8685
Daniel J. Pritchett <dpritchett@gmail.com>
2013-02-14 10:26:49 -0600

    Added pg user and db creation recipe

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,13 @@ maintainer_email "tlowrimore@coroutine.com"
 license          "Apache 2.0"
 description      "Configures Rails Apps with Apache/Passenger/RVM"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.8"
+version          "0.1.9"
 
 recipe           "rails_apps::default", "Installs Apache, RVM, and Passenger"
 recipe           "rails_apps::setup", "Creates directory structure for Rails apps"
 recipe           "rails_apps::apache_config", "Configures Apache, Passenger"
 recipe           "rails_apps::logrotate", "Sets up logrotate for Rails logs"
+recipe           "rails_apps::create_pg_users_and_dbs", "Creates Postgresql objects for Rails apps"
 
 depends "apache2"
 depends "rvm"
---
+++ recipes/create_pg_users_and_dbs.rb
@@ -0,0 +1,104 @@
+#
+# Cookbook Name:: rails_apps
+# Recipe:: create_pg_users_and_dbs
+#
+# Copyright 2013, Coroutine LLC
+#
+# --------------------------------------
+# Sample Item from the specified Databag
+# --------------------------------------
+# {
+#    "id": "postgresql_setup_wfp",
+#    "users": [
+#        {
+#            "username":"some_user",
+#            "password":"some_password",
+#            "superuser": "true",
+#        }
+#    ],
+#    "databases": [
+#        {
+#            "name":"some_db",
+#            "owner":"some_user",
+#            "template":"template0",
+#            "encoding": "UTF8",
+#            "locale": "en_US.utf8"
+#        }
+#    ]
+# }
+# --------------------------------------
+
+
+# Fetch the setup items from the Databag; It contains things like Database users,
+# passwords, DB names and encoding.
+setup_items = []
+node['rails_apps']['pg_setup_items'].each do |itemname|
+  databag = node['rails_apps']['databag']
+
+  # NOTE: shared secret must be in "/etc/chef/encrypted_data_bag_secret"
+  Chef::Log.info("fetching #{dbag_item} from Encrypted 'rails_apps' data bag")
+  i = Chef::EncryptedDataBagItem.load("rails_apps", dbag_item)
+  Chef::Log.info("found #{i}")
+  setup_items << i
+end
+
+# We use a mix of psql commands and SQL statements to create users.
+#
+# To Create a User:
+#     sudo -u postgres createuser -s some_user
+#
+# To set their password:
+#     sudo -u postgres psql -c "ALTER USER some_user WITH PASSWORD 'secret';"
+#
+# To create a Database
+#     sudo -u postgres createdb -E UTF8 -O some_user \
+#          -T template0 database_name --local=en_US.utf8
+#
+# To make these idempotent, we test for existing users/databases;
+# Test for existing DB:
+#     sudo -u postgres psql -l | grep database_name
+#
+# Test for existing Users
+#     sudo -u postgres psql -c "\du" | grep some_user
+
+setup_items.each do |setup|
+
+  setup["stages"].each do |stage|
+    db_config = stage.fetch('database')
+    user_name  = db_config.fetch('username')
+    db_name   = db_config.fetch('dbname')
+
+    create_user_command = begin
+        ["sudo -u postgres createuser #{user_name}",
+        " --no-superuser", "--no-createdb", "--no-createrole"].join(' ')
+    end
+
+    set_user_password = begin
+        "sudo -u postgres psql -c \"ALTER USER #{user_name} " +
+        "WITH PASSWORD '#{db_config.fetch('password')}';\""
+    end
+
+    bash "create_user" do
+      user "root"
+      code <<-EOH
+        #{create_user_command} #{set_user_password}
+      EOH
+      not_if "sudo -u postgres psql -c \"\\du\" | grep #{user_name}"
+    end
+
+    create_database_command = begin
+      ["sudo -u postgres createdb #{db_name}",
+        "--owner=#{user_name} ", "--encoding=#{db_config.fetch('encoding')}",
+        "--locale=#{db_config.fetch('locale')}", "--template=#{db_config.fetch('template')}"].join(' ')
+    end
+
+    bash "create_database" do
+      user "root"
+      code <<-EOH
+        #{create_database_command}
+      EOH
+      not_if "sudo -u postgres psql -l | grep #{db_name}"
+    end
+  end
+
+end
